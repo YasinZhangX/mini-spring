c3652219cc3aac0eefb4a5279ae916d59df40329
Yasin <271155397@qq.com>
2019-06-24 00:29:38 +0800

    init

---
+++ framework/src/main/java/com/yasin/starter/MiniApplication.java
@@ -0,0 +1,12 @@
+package com.yasin.starter;
+
+/**
+ * @author yasin
+ */
+public class MiniApplication {
+
+    public static void run(Class<?> cls, String[] args) {
+        System.out.println("Hello, Mini-Spring");
+    }
+
+}
---
+++ test/src/main/java/com/yasin/Application.java
@@ -0,0 +1,15 @@
+package com.yasin;
+
+import com.yasin.starter.MiniApplication;
+
+/**
+ * @author yasin
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        MiniApplication.run(Application.class, args);
+    }
+
+}
