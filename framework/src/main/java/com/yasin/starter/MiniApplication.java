fbb37438055b45d31073ef728602b4573fa1abbe
yasin <271155397@qq.com>
2019-06-24 16:54:45 +0800

    sync

--- framework/src/main/java/com/yasin/starter/MiniApplication.java
+++ framework/src/main/java/com/yasin/starter/MiniApplication.java
@@ -1,5 +1,8 @@
 package com.yasin.starter;
 
+import com.yasin.web.server.TomcatServer;
+import org.apache.catalina.LifecycleException;
+
 /**
  * @author yasin
  */
@@ -7,6 +10,12 @@ public class MiniApplication {
 
     public static void run(Class<?> cls, String[] args) {
         System.out.println("Hello, Mini-Spring");
+        TomcatServer tomcatServer = new TomcatServer(args);
+        try {
+            tomcatServer.startServer();
+        } catch (LifecycleException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- framework/src/main/java/com/yasin/web/server/TomcatServer.java
+++ framework/src/main/java/com/yasin/web/server/TomcatServer.java
@@ -1,9 +1,33 @@
 package com.yasin.web.server;
 
+import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 
+/**
+ * @author yasin
+ */
 public class TomcatServer {
 
     private Tomcat tomcat;
+    private String[] args;
+
+    public TomcatServer(String[] args) {
+        this.args = args;
+    }
+
+    public void startServer() throws LifecycleException {
+        tomcat = new Tomcat();
+        tomcat.setPort(8088);
+        tomcat.start();
+
+        Thread awaitThread = new Thread("tomcat_await_thread") {
+            @Override
+            public void run() {
+                TomcatServer.this.tomcat.getServer().await();
+            }
+        };
+        awaitThread.setDaemon(false);
+        awaitThread.start();
+    }
 
 }
